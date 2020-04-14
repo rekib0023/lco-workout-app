ad47b020ed0c063dcf0f3b3f4683b1b2ca1100f2
rekib0023 <rkb.ra0025@gmail.com>
2020-04-14 19:15:31 +0530

    switched to gridview, removed slivers all together

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lco_workout/screens/workouts/workoutList.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final workouts = WorkoutDetails().workoutList;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-    );
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: GridView.builder(
+          itemCount: workouts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20.0,
+            mainAxisSpacing: 20.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                print(workouts[index]);
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Container(
+                  color: Colors.amberAccent,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.white,
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: Image.asset(workouts[index]['image']),
+                      ),
+                      Text(workouts[index]['name']),
+                      Text(workouts[index]['desc']),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
